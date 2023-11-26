74e7df4e0ce7e3cea330f6862f271f096549b8a8
Juan Siliezar <juansiliezar@Juans-Air.lan>
2023-11-26 15:22:30 -0500

    update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import plotly.express as px
 import requests
 from io import BytesIO
 
-# -------------------------------------------------------------
+# 
 # trying to read in dataset from google sheet i made public 
 # and then loading it from the project directory if it fails
-# -------------------------------------------------------------
+# 
 try:  
     sheet_url = "https://docs.google.com/spreadsheets/d/1jzihXIadik_fkLF3u5dtNW2ELG0Ts3CNjOMFAVnrPL0/edit?usp=sharing"
     r = requests.get(sheet_url)   
@@ -16,13 +16,20 @@ try:
 except:   
     df = pd.read_csv("datasets/vehicles_us.csv")    
 
-st.title('Analyzing a Sample of the Used Car Market in the US')
-st.header('Data Viewer')
+# 
+# Adding a title to the page
+# 
+st.title('Analyzing a Sample of Used Car Market in the US')
+
+#
+# and displaying the dataset with a header labeling it
+# 
+st.header('Dataset')
 st.write(df)
 
-# -------------------------------------------------------------
-# plotting the distribution of key variables with histograms
-# -------------------------------------------------------------
+# 
+# Visualizing Distribution of Vehicle Price with a Histogram
+# 
 price_hist = px.histogram(
     df, 
     x='price', 
@@ -31,6 +38,15 @@ price_hist = px.histogram(
     labels={'price' : 'Vehicle Price'}
     )
 
+# 
+# Displaying Plot
+# 
+st.plotly_chart(price_hist, use_container_width=True)
+st.divider()
+
+# 
+# Visualizing Distribution of Model Year with a Histogram
+# 
 model_year_hist = px.histogram(
     df, 
     x='model_year', 
@@ -39,6 +55,15 @@ model_year_hist = px.histogram(
     labels={'model_year' : 'Model Year'}
     )
 
+# 
+# Displaying Plot
+# 
+st.plotly_chart(model_year_hist, use_container_width=True)
+st.divider()
+
+# 
+# Visualizing Distribution of Vehicle Mileage with a Histogram
+# 
 odom_hist = px.histogram(
     df, 
     x='odometer', 
@@ -47,6 +72,15 @@ odom_hist = px.histogram(
     labels={'odometer' : 'Vehicle Mileage'}
     )
 
+# 
+# Displaying Plot
+# 
+st.plotly_chart(odom_hist, use_container_width=True)
+st.divider()
+
+# 
+# Visualizing Distribution of Days on Market with a Histogram
+#
 dom_hist = px.histogram(
     df, 
     x='days_listed', 
@@ -54,20 +88,43 @@ dom_hist = px.histogram(
     title='Distribution of Days on Market', 
     labels={'days_listed' : 'Days on Market'}
     )
+#
+# Displaying Plot
+# 
+st.plotly_chart(dom_hist, use_container_width=True)
+st.divider()
 
-# -------------------------------------------------------------
-# Visualizng potential relationships between key variables
-# -------------------------------------------------------------
-
+# 
+# Creating Price vs. Odometer Scatter Plot
+#
 price_vs_odometer = px.scatter(
     df, 
     x='odometer', y='price', 
     title='Price vs. Vehicle Mileage', 
     labels={'odometer' : 'Mileage', 
             'price' : 'Price (USD)'},
-    log_x=False, log_y=False
+    log_x=log_x, log_y=log_y
     )
-
+#
+# Adding a checkbox option to view graph's axes scaled logarithmically
+# and adjusting the corresponding parameters depending on input
+#
+log = st.checkbox('View Log-Scaled Axes')
+if log:
+    log_x = True
+    log_y = True
+else:
+    log_x=False
+    log_y=False
+#
+# Displaying Plot
+#    
+st.plotly_chart(price_vs_odometer, use_container_width=True)
+st.divider()
+ 
+# 
+# Creating Price vs. Model Year Scatter Plot
+#   
 price_vs_model_year = px.scatter(
     df, 
     x='model_year', y='price', 
@@ -77,23 +134,23 @@ price_vs_model_year = px.scatter(
     log_x=False, log_y=False
     )
 
-st.plotly_chart(price_hist, use_container_width=True)
-st.divider()
-
-st.plotly_chart(model_year_hist, use_container_width=True)
-st.divider()
-
-st.plotly_chart(odom_hist, use_container_width=True)
-st.divider()
-
-st.plotly_chart(dom_hist, use_container_width=True)
-st.divider()
-
-log = st.checkbox('Log-Scaled Axes')
+# 
+# Adding a checkbox option to view graph's axes scaled logarithmically
+# and adjusting the corresponding parameters depending on input
+# 
+log = st.checkbox('View Log-Scaled Axes')
 if log:
     log_x = True
     log_y = True
-    
-st.plotly_chart(price_vs_odometer, use_container_width=True)
-st.divider()
+else:
+    log_x=False
+    log_y=False
+
+# 
+# Displaying Plot
+# 
 st.plotly_chart(price_vs_model_year, use_container_width=True)
+
+
+
+    
